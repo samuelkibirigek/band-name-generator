7aca0108286caf7ed9e6b065f222f1d4b49dad7c
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-31 15:04:54 +0300

    The band name generator program.

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!\n")
+city = input("Whats the name of the place you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print(f"Your band could be {city} {pet}.")
